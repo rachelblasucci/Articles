5166f5a47e54b6246ac7d50701efd94b41346926
Rachel Reese <rachel.reese@fireflylogic.com>
2014-02-20 18:15:27 -0600

    The Developer - NDC - Feb 2014
    Creating Responsive UIs with F#

---
+++ NDC/AwaitObservable/FormatTextModel.fs
@@ -0,0 +1,16 @@
+﻿module FormatTextModel
+
+open System.Threading
+
+let private isVowel isWvowel isYvowel character = 
+    let isV = match character with 
+                | 'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U'-> true
+                | 'w' | 'W' when isWvowel -> true
+                | 'y' | 'Y' when isYvowel -> true
+                | _ -> false
+    (character, isV)
+    
+let CheckForVowels isWvowel isYvowel text = async {
+        Thread.Sleep(500)
+        return Array.map (isVowel isWvowel isYvowel) text
+    }
---
+++ NDC/Events/App.fs
@@ -0,0 +1,23 @@
+﻿module MainApp
+
+open System
+open System.Windows
+open System.Windows.Controls
+open FSharpx
+
+type MainWindow = XAML<"MainWindow.xaml">
+
+let window = MainWindow()
+let _viewModel = new FormatTextViewModel.FormatTextViewModel()
+
+let loadWindow() =
+    _viewModel.PropertyChanged 
+        |> Event.add (fun _ -> window.FormattedTextBox.Document <- _viewModel.GetFormattedText)
+
+    window.UnformattedTextBox.KeyUp.Add(fun _ -> _viewModel.Text <- window.UnformattedTextBox.Text)
+
+    window.Root.DataContext <- _viewModel
+    window.Root
+
+[<STAThread>]
+(new Application()).Run(loadWindow()) |> ignore
